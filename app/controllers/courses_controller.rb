a79abf202b5eb42bf1717ca1c7876af26567db37
mohamed_mamdouh <mm95mido@gmail.com>
2015-12-13 23:18:44 +0200

    app with style

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   def index
-  	@search_term='jhu'
+  	@search_term = params[:looking_for]||'ruby'
   	@courses = Coursera.look(@search_term)
   end
 end
