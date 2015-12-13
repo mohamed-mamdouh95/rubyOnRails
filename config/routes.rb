06a70a34516a4b9ca0508618bfb72af08751df94
mohamed_mamdouh <mm95mido@gmail.com>
2015-12-13 19:43:17 +0200

    app without styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'courses/index'
+
   get 'greeter/hey'
   get 'greeter/finish'
-
+  get 'courses/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
